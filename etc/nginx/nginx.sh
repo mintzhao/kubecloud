a8cb2b8783eb6adaab28df545141d7da1f7b0798
zhaoming <zhaoming@cloudin.kmail.com>
2017-05-02 09:40:08 +0800

    add efk build script

--- etc/nginx/nginx.sh
+++ etc/nginx/nginx.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
 
-docker run --name mintNGINX -p 35580:35580  -v /root/mintNGINX/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
+docker rm -f mintNGINX
+docker run --name mintNGINX -p 35580:35580 -p 35581:35581 -v /root/mintNGINX/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
